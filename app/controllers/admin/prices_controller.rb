3cdc404212ae49802d4dd716319304d6a6670e74
tolves <tov.sharing@gmail.com>
2020-11-15 12:45:29 +0000

    brands, categories, prices

---
+++ app/controllers/admin/prices_controller.rb
@@ -0,0 +1,2 @@
+class Admin::PricesController < ApplicationController
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ApplicationRecord
   # validates :categories
   # validates :types
   # validates :detail
+
+  # belongs_to :brand  add_reference :products, :brand t.belongs_to :author
+  # belongs_to :category
+  # has_many :price
 end
